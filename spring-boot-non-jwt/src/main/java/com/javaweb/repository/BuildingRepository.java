cf9553e5ff657b64efb30ab0f7ad353a08aec048
minhtuan1102 <apointminhtuan@gmail.com>
2025-09-17 21:28:48 +0700

    Fix Repo

--- spring-boot-non-jwt/src/main/java/com/javaweb/repository/BuildingRepository.java
+++ spring-boot-non-jwt/src/main/java/com/javaweb/repository/BuildingRepository.java
@@ -4,12 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import org.antlr.v4.runtime.misc.TestRig;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.javaweb.builder.BuildingSearchBuilder;
 import com.javaweb.repository.custom.BuildingRepositoryCustom;
 import com.javaweb.repository.entity.BuildingEntity;
 
+@Repository
 public interface BuildingRepository extends JpaRepository<BuildingEntity, Long>, BuildingRepositoryCustom {
 	void deleteByIdIn(Long[] ids);
+	
+	List<BuildingEntity> findByNameContaining(String s);
+	List<BuildingEntity> findByNameContainingAndStreet(String name, String street);
 }
